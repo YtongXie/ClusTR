6794b689585c711e620c735adac52738f1c5728d
YtongXie <44012746+YtongXie@users.noreply.github.com>
2023-04-05 11:01:48 +0930

    Add files via upload

---
+++ configs/ClusTR/ClusTR_b2.py
@@ -0,0 +1,6 @@
+cfg = dict(
+    model='ClusTR_b2',
+    drop_path=0.1,
+    clip_grad=None,
+    output_dir='checkpoints/ClusTR_b2',
+)
---
+++ configs/ClusTR/ClusTR_b3.py
@@ -0,0 +1,6 @@
+cfg = dict(
+    model='ClusTR_b3',
+    drop_path=0.3,
+    clip_grad=1.0,
+    output_dir='checkpoints/ClusTR_b3',
+)
